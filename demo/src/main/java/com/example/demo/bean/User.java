9410a3bb9ee9b1fe78ed4f9d85b48311c9e85ef9
Yuan Liu <yuanliu2@cisco.com>
2020-04-29 11:22:40 +0800

    spring boot mybatis demo

---
+++ demo/src/main/java/com/example/demo/bean/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.bean;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private double money;
+}
